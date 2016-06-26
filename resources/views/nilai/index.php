7bbee65d19f1d048753b8a412a3d408127ebc7de
selesdepselesnul <selesdepselesnul@gmail.com>
2016-06-26 13:37:06 +0700

    add loading animation when waiting for grade

--- resources/views/nilai/index.php
+++ resources/views/nilai/index.php
@@ -33,12 +33,19 @@
             </tbody>    
         </table>
     </div>
+    <div class="modal"></div>
 </div>
 <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 <script src="js/underscore-min.js"></script>
 <script type="text/javascript">
+    var body = $("body");
+
+    $(document).on({
+        ajaxStart: function() { body.addClass("loading");    },
+         ajaxStop: function() { body.removeClass("loading"); }    
+    });
     $('#npmSearchInput').on('change keyup', function() {
         var npm = $('#npmSearchInput').val();
         if (npm.length == 14) {
