36e07515e2604806d5f0ecdbef0bcd0e0a76f72c
selesdepselesnul <selesdepselesnul@gmail.com>
2016-06-28 17:14:48 +0700

    refactor ui for profile

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,35 +1,62 @@
 @extends('master')
-@section('content')
-<div id="profileText" class="center-block">
-    
-</div>
+
+@section('uppercustomjs')
 @endsection
+
+@section('content')
+<h1 class="visible-md-block visible-lg-block"><u>Profile</u></h1>
+<h4 class="visible-sm-block"><u>Profile</u><h4>
+<h6 class="visible-xs-block"><u>Profile</u></h6>
+<div class="modal"></div>
+<div id="profileText" class="center-block"></div>
+@endsection
+
 @section('customjs')
 <script type="text/javascript">
+$('#profileText').hide();
+var body = $("body");
+$(document).on({
+    ajaxStart: function() { body.addClass("loading");    },
+    ajaxStop: function() { 
+        body.removeClass("loading"); 
+        $('#profileText').show();
+    }    
+});
+function checkIfNull(data) {
+    return data == '' || data == null ? '-' : data;
+}
 $(document).ready(function() {
-    $('#profileText').hide();
-    var body = $("body");
-    $(document).on({
-        ajaxStart: function() { body.addClass("loading");    },
-        ajaxStop: function() { 
-            body.removeClass("loading"); 
-            $('#profileText').show();
-        }    
-    });
+
     $.get(
         "http://www.siakapi.selesdepselesnul.com/profile/npm/"+{{ $npm }}, 
         function(profile) {
             var studentProfile = profile.data;
-            if(studentProfile) {
-                var profileText = _.each(studentProfile, function(val, key) {
-                    $('#profileText').append('<p>' + key + ' : ' + val + '</p>');    
-                });
-                
+            if(studentProfile == null) {
+                 $('#profileText').append(
+                    '<p class="visible-xs-block" style="font-size: 14px">yang bersangkutan tidak berkenan profilenya ditampilkan<br/>'
+                    +'anda tidak berkenan ?<br/>'
+                    +'hubungi : <a href="mailto:selesdepselesnul@gmail.com?Subject=TidakBerkenan" target="_top">selesdepselesnul@gmail.com</a></p>');
+                 $('#profileText').append(
+                    '<p class="visible-sm-block" style="font-size: 20px">yang bersangkutan tidak berkenan profilenya ditampilkan<br/>'
+                    +'anda tidak berkenan ?<br/>'
+                    +'hubungi : <a href="mailto:selesdepselesnul@gmail.com?Subject=TidakBerkenan" target="_top">selesdepselesnul@gmail.com</a></p>');
+                $('#profileText').append(
+                    '<p class="visible-md-block visible-lg-block" style="font-size: 40px">yang bersangkutan tidak berkenan profilenya ditampilkan<br/>'
+                    +'anda tidak berkenan ?<br/>'
+                    +'hubungi : <a href="mailto:selesdepselesnul@gmail.com?Subject=TidakBerkenan" target="_top">selesdepselesnul@gmail.com</a></p>');
                 console.log(profileText);
+            } else {
+                var profileText = _.each(studentProfile, function(val, key) {
+                    $('#profileText').append(
+                        '<p class="visible-xs-block" style="font-size: 14px">' + key + ' : ' + checkIfNull(val) + '</p>'); 
+                    $('#profileText').append(
+                        '<p class="visible-sm-block" style="font-size: 20px">' + key + ' : ' + checkIfNull(val) + '</p>');       
+                    $('#profileText').append(
+                        '<p class="visible-md-block visible-lg-block" style="font-size: 40px">' + key + ' : ' + checkIfNull(val) + '</p>');    
+                });
             }
         }, 
         "json");    
 });    
-
 </script>
 @endsection
